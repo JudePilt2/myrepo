f1fa81cb0c4852e8f582af52f3dff6c6a4622a0a
JudePilt2 <jpiltingsrud@luc.edu>
2025-02-22 17:18:59 -0600

    Retrieves paired-end fastq files from two SSR numbers

---
+++ retrieve.py
@@ -0,0 +1,28 @@
+import sys, argparse, os
+from Bio import SeqIO
+
+#Function to parse command line arguments
+def check_arg(args=None):
+    parser = argparse.ArgumentParser(
+    description="Retrieve and process SSR information")
+    parser.add_argument("-i", "--input",
+    help="input file",
+    required=True)
+    return parser.parse_args(args)
+
+#retrieve command line arguments
+arguments = check_arg(sys.argv[1:])
+infile = arguments.input
+
+i=0
+with open (infile, 'r') as f:
+    file=f.read().split() #reads in lines removing spaces and \n values
+
+while i<2:
+    getfile=('wget https://trace.ncbi.nlm.nih.gov/Traces/sra-reads-be/fastq?acc='+file[i])
+    name=('mv fastq?acc='+file[i]+' '+file[i])
+    split=('fastq-dump --split-files '+file[i])
+    os.system(getfile)
+    os.system(name)
+    os.system(split)
+    i+=1
