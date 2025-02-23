949d8eecebfd35cb4c265f0f726836603e316b10
JudePilt2 <jpiltingsrud@luc.edu>
2025-02-22 20:57:57 -0600

    Generates paired-end fastq files for two SSRs, creates a working directory, and formats files for wrapper.py

--- retrieve.py
+++ retrieve.py
@@ -27,19 +27,12 @@ else:
 
 i=0 #empty counter for file
 while i<2:
-    getfile=('wget https://trace.ncbi.nlm.nih.gov/Traces/sra-reads-be/fastq?acc='+file[i]) #retrieves each SSR file in .gz format
-    name=('mv fastq?acc='+file[i]+' '+file[i]) #renames default download name
-    split=('fastq-dump --split-files '+file[i]) #splits .gz into two paired-end fastq files 
-    os.system(getfile) #calling the commands
-    os.system(name)
-    os.system(split)
+    os.system('wget https://trace.ncbi.nlm.nih.gov/Traces/sra-reads-be/fastq?acc='+file[i]) #retrieves each SSR file in .gz format
+    os.system('mv fastq?acc='+file[i]+' '+file[i]) #renames default download name
+    os.system('fastq-dump --split-files '+file[i]) #splits .gz into two paired-end fastq files
     i+=1
 
-name1=('mv '+file[0]+'_1.fastq'+' SSR1_1.fastq') #renames processed .fastq files to work with wrapper.py
-name2=('mv '+file[0]+'_2.fastq'+' SSR1_2.fastq')
-name3=('mv '+file[1]+'_1.fastq'+' SSR2_1.fastq')
-name4=('mv '+file[1]+'_2.fastq'+' SSR2_2.fastq')
-os.system(name1) #calls commands
-os.system(name2)
-os.system(name3)
-os.system(name4)
+os.system('mv '+file[0]+'_1.fastq'+' SSR1_1.fastq') #renames processed .fastq files to work with wrapper.py
+os.system('mv '+file[0]+'_2.fastq'+' SSR1_2.fastq')
+os.system('mv '+file[1]+'_1.fastq'+' SSR2_1.fastq')
+os.system('mv '+file[1]+'_2.fastq'+' SSR2_2.fastq')
